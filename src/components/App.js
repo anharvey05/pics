2979b9d3b3e16d1da6dd6f743c25953af19e4dd6
anharvey05 <amy.harvey@onshoreoutsourcing.com>
2019-10-01 08:34:35 -0500

    Through part 93

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,17 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-    onSearchSubmit(term) {
-        axios.get('https://api.unsplash.com/search/photos', {
+    state = { images: [] };
+    
+    onSearchSubmit = async (term) => {
+        const response = await axios.get('https://api.unsplash.com/search/photos', {
             params: { query: term },
             headers: {
                 Authorization: 'Client-ID ec558d4d9fe7a50eafc43d7d535af82ac4d19e1779d7f8ab0becd9386b02d804'
             }
         });
+
+        this.setState({ images: response.data.results});
     }
     
     
@@ -17,6 +21,7 @@ class App extends React.Component {
         return (
             <div className="ui container" style={{marginTop: '10px'}}>
                 <SearchBar onSubmit={this.onSearchSubmit} />
+                Found:  {this.state.images.length} images
             </div>
         );
     }
